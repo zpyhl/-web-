f37396fb08275c9c6e66b861275f1a9c41e21aba
hanlei <hl@qq.com>
2019-04-09 21:55:23 +0800

    lunbotu info book

--- weijinsuoProject/js/index.js
+++ weijinsuoProject/js/index.js
@@ -1,5 +1,6 @@
 $(function(){
     banner();
+    initTab()
 });
 
 // 动态响应式轮播图
@@ -8,26 +9,26 @@ function banner(){
     var imageList = [
         {
             pcImg:'images/slide_01_2000x410.jpg',
-            mIMg:'images/slide_01_640x340.jpg'
+            mImg:'images/slide_01_640x340.jpg'
         },
         {
             pcImg:'images/slide_02_2000x410.jpg',
-            mIMg:'images/slide_02_640x340.jpg'
+            mImg:'images/slide_02_640x340.jpg'
         },
         {
             pcImg:'images/slide_03_2000x410.jpg',
-            mIMg:'images/slide_03_640x340.jpg'
+            mImg:'images/slide_03_640x340.jpg'
         },
         {
             pcImg:'images/slide_04_2000x410.jpg',
-            mIMg:'images/slide_04_640x340.jpg'
+            mImg:'images/slide_04_640x340.jpg'
         }
     ];
 //2.渲染的方法
     var renderHtml = function(){
         // 判断渲染设备
         var width = $(window).width();
-        var isMobile = width >= "768" ? true : false ;
+        var isMobile = width >= 768 ? false : true ;
         var pointStr = $("#point_template").html();
         var imageStr = $("#image_template").html();
         var pointFuc = _.template(pointStr);
@@ -38,5 +39,28 @@ function banner(){
         $(".carousel-inner").html(imageHtml);
 
     };
-    renderHtml();
+    $(window).on('resize',function(){
+        renderHtml();
+    }).trigger('resize');
+
+}
+function initTab(){
+    // 1计算ul宽度，设置好
+    //2父容器设置成溢出隐藏的盒子
+    //3初始化滑动组块
+    var $parent = $('.wjs_product_box');
+    var $child = $parent.find('ul');
+    var $lis = $child.find("li");
+    var width = 0;
+    $lis && $lis.each(function(){
+        width += $(this).outerWidth(true);
+    });
+    $child.width(width);
+    // 3初始化滑动组块
+    itcast.iScroll({
+        swipeDom:$parent.get(0),
+        swipeType:'x',
+        swipeDistance:'50'
+    });
+
 }
